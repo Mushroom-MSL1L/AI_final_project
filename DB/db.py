2712c94fa956bbb9bdd3914ec5db67ba9c1a6d02
Mushroom-MSL1L <mushroom.ac.cs11@nycu.edu.tw>
2024-05-19 00:27:27 +0800

    refactor : new db class (recommend)
    1. change the entire db class

--- DB/db.py
+++ DB/db.py
@@ -1,43 +1,56 @@
-from langchain_community.vectorstores import Chroma
-from langchain_community.embeddings import LlamaCppEmbeddings
-import sys
 import os
+import sys
+import chromadb
+from chromadb.utils import embedding_functions
 
 from api import API
 current_dir_path = os.path.dirname(os.path.realpath(__file__))
 sys.path.insert(0, current_dir_path)
 
-class db :
-    def __init__(self, persistant_directory = os.path.join(current_dir_path, "data")) :
-        self.project_root_path = os.path.dirname(current_dir_path)
-        self.llama_model_path = os.path.join(self.project_root_path, "llama.cpp", "models", "llama-2-7b", "llama-2-7b-7B-F32-q4_0.gguf")
-        self.persistant_directory = self.set_persistant_directory(persistant_directory)
-        self.embeddings = LlamaCppEmbeddings(model_path=self.llama_model_path)
-        self.chroma_db = Chroma(
-                persist_directory=persistant_directory, 
-                embedding_function=self.embeddings,
-                collection_name="lc_chroma_demo")
-        self.api = API()
+class db ():
+    def __init__(self, 
+                persistent_path=os.path.join(current_dir_path, 'data'), 
+                collection_name='steam_reviews',
+                embedding_func=embedding_functions.SentenceTransformerEmbeddingFunction()
+                ):
+        self.persistent_path = persistent_path
+        self.collection_name = collection_name
+        self.chroma_client = chromadb.PersistentClient(persistent_path)
+        self.embedding_func = embedding_func
+        self.collection = self.chroma_client.get_or_create_collection(
+            name=collection_name,
+            embedding_function=self.embedding_func,
+        )
+        self.a = API()
 
-    def set_persistant_directory(self, persistant_directory) :  
-        if not os.path.exists(persistant_directory):
-            os.makedirs(persistant_directory)
-        return persistant_directory
+    def add_reviews(self, game_name):
+        game_id = self.a.get_game_Id(game_name)
+        r = self.a.get_reviews(game_id)
+        self.collection.add (
+            ids=[str(game_id)+"_"+str(i) for i in range(len(r))],
+            documents=r,
+            metadatas=[{'game_id': game_id, 'game_name':game_name} for i in range(len(r))],
+        )
+
+    def get_query(self, game_name, query, n=5):
+        return self.collection.query(
+            where={'game_name': game_name},
+            query_texts = query,
+            n_results=n)
+            
+    def get_query_text(self, game_name, query, n=5):
+        return self.get_query(game_name, query, n)['documents'][0]
     
-    def add_reviews(self, game_name) :
-        game_id = self.api.get_game_Id(game_name)
-        r = self.api.get_reviews(game_id)
-        self.chroma_db.add_texts(
-            texts=r,
-        )
-        self.chroma_db.persist()
-
-    def get_similar_texts (self, query, n=5) :
-        return self.chroma_db.get_similar_texts(
-            query=query,
-            n=n
-        )
+    def get_query_by_embeddings(self, embeddings, n=5):
+        return self.collection.query(
+            query_embeddings=embeddings, 
+            n_results=n)
+    
+    def get_collection(self):
+        return self.collection()
     
 # DB = db()
-# DB.add_reviews("Forza Horizon 5")
-# print(DB.get_similar_texts("I love the game", 5))
+# DB.add_reviews('Forza Horizon 4')
+# DB.add_reviews('Stardew Valley')
+# print(DB.get_query_text('Forza Horizon 4', 'fun game', n=5))
+
