551a0dc34b06e59e3d88f6548b7b179ab3172cf8
WilsonNYCU <166817905+WilsonNYCU@users.noreply.github.com>
2024-06-01 02:58:53 +0800

    Update ui.py

--- ui.py
+++ ui.py
@@ -1,23 +1,24 @@
 import chainlit as cl
+import model
 
-from Model import Chain
-from Model import LLM
+chain = model.Chain()
 
-chain = Chain()
+@cl.on_chat_start
+async def main():
+    res = await cl.AskUserMessage(content="Welcome to Steam-Hot-Comment-Fetcher (SHCT)!\nPlease type your name.", timeout=30).send()
+    if res:
+        await cl.Message(
+            content=f"Your name is: {res['output']}.\nChainlit installation is working!\nYou can now start typing the game name!",
+        ).send()
 
-# chainlit run ui.py -w
-# type above command in terminal to run this file
-
-# @cl.on_chat_start
-# async def main():
-#     res = await cl.AskUserMessage(content="What is the game name?", timeout=30).send()
-#     if res:
-#         await cl.Message(
-#             content=f"Your name is: {res['content']}.\nChainlit installation is working!\nYou can now start building your own chainlit apps!",
-#         ).send()
 
 @cl.on_message
 async def main(message: str):
     await cl.Message(
-        content=chain(message.content),
+            # content=f"The game name is: {message.content}.",
+            content = chain(message.content),
     ).send()
+
+@cl.on_chat_end
+def on_chat_end():
+    print("The user disconnected!")
